8770d2c8ab41a34ee79e69354c77399c528d0840
steeven-louk <steevenlouk.sl@gmail.com>
2022-09-17 22:50:53 +0200

    mise en place du responsive

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -14,7 +14,10 @@ export const Navbar = (props) => {
     
     <div className="navbar_component">
        <div className="nav">
-        <span className="nav-brand">intra-net </span>
+        <div className="nav-brand">
+          <span>congo</span>
+          <small>consulting</small>
+        </div>
             <div className="input-box">
             <FontAwesomeIcon className='icon'  icon="fa-solid fa-magnifying-glass" /><input type="text" placeholder='Recherchez un colaborateur' value={props?.search} onChange={(e)=>props?.setSearch(e.target.value)}/>
             </div>
