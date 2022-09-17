e9c4e88317a36e7c0a4c6fb17b2ee2e5c363e638
steeven-louk <steevenlouk.sl@gmail.com>
2022-09-17 16:45:18 +0200

    mise en place de la page home

---
+++ client/src/views/home/index.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+import './style/style.scss'
+
+import { Card } from "../../components/card/Card";
+
+import { getRandomCollaborateur } from "../../services/service";
+
+export const Index = () => {
+
+
+   const [getRandom, setGetRandom] = useState(null);
+
+
+const randoms = () =>{
+    getRandomCollaborateur().then((data)=>{
+      setGetRandom(data.data);
+  })
+}
+
+useEffect(() => {
+  randoms();
+}, []);
+
+
+  return (
+
+    <div className="homepage">
+      <div className="homepage__container">
+        <h1>Bienvenue sur l'intranet</h1>
+        <p>
+          La plate-forme de l'entreprise qui vous permet de retrouver tous vos
+          collaborateur.
+        </p>
+        <p>avez-vous dit bonjour à:</p>
+
+        <div className="card">
+          <Card item={getRandom}/>
+        </div>
+
+        <button onClick={()=>randoms()} className="btn">Dire bonjour à quelqu'un d'autre</button>
+      </div>
+    </div>
+  );
+};
